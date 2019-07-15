084647f36933ebe52adb26f3d240182490cc97ac
Sandors <382524646@qq.com>
2019-07-15 17:31:44 +0800

    django test

--- untitled2/urls.py
+++ untitled2/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     # re_path(r'^blog/', include('templates.urls')),
     re_path(r'^archives/([0-9]{4}/$)',views.year_archive,name='news-year-archive'),
-    #####
+    ######
 
 ]
 
