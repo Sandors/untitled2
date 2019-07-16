5b6d03e3815c46e77d1062bdc45cdab1813cfb25
Sandors <382524646@qq.com>
2019-07-16 17:29:55 +0800

    django push test

--- my_first_django/views.py
+++ my_first_django/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect,redirect
+import datetime
 
 # Create your views here.
 
 def index(request):
 	import datetime
-	now = datetime.datetime.now()
-	ctime = now.strftime("%Y-%m-%d %X")
+	S = "hello world"
+	L = [111,222,333]
+	dic = {"name":"yuan","age":18}
+	date = datetime.date(1993,4,5)
+	class Person(object):
+		def __init__(self,name):
+			self.name=name
+	person_yuan = Person("yuan")
+	person_egon = Person("egon")
+	person_alex = Person("alex")
 
-	return render(request,"index.html",{"ctime":ctime})
+	persion_list = [person_yuan,person_egon,person_alex]
+	value = "<a href="">点击</a>"
+
+	return render(request,'index.html',{"S":S,"L":L,"dic":dic,"date":date,"persion_list":persion_list,"value":value})
+
 
 def special_case_2003(request):
 	import datetime
@@ -45,5 +60,36 @@ def article_detail(requset,year,month,day):
 
 def redirect_to_year(request):
 
-	year=2006
-	return  HttpResponseRedirect(reversed('news-year-archive',args=(year,)))
+	year = 2006
+	return  HttpResponseRedirect(reverse('news-year-archive',args=(year,)))
+
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body>It is now %s.</body></html>" % now
+	return HttpResponse(html)
+
+def moban(request):
+
+    import datetime
+    now=datetime.datetime.now()
+    ctime=now.strftime("%Y-%m-%d %X")
+
+    return render(request,"base.html",{"ctime":ctime})
+
+
+def moban1(request):
+
+    import datetime
+    now=datetime.datetime.now()
+    ctime=now.strftime("%Y-%m-%d %X")
+
+    return render(request,"jicheng1.html",{"ctime":ctime})
+
+def moban2(request):
+
+    import datetime
+    now=datetime.datetime.now()
+    ctime=now.strftime("%Y-%m-%d %X")
+
+    return render(request,"jicheng2.html",{"ctime":ctime})
--- untitled2/urls.py
+++ untitled2/urls.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 from django.urls import path,re_path,include
 
 from my_first_django import views
-from django.conf import urls
+from django.conf.urls import url
 
 
 urlpatterns = [
 
     # re_path(r'^blog/', include('templates.urls')),
     re_path(r'^archives/([0-9]{4}/$)',views.year_archive,name='news-year-archive'),
+    re_path(r'time/',views.current_datetime),
+    re_path(r'index/',views.index),
+    re_path(r'moban/',views.moban),
+    re_path(r'moban1/',views.moban1),
+    re_path(r'moban2/',views.moban2),
 
-#111111
+
 ]
 
