3aa7cd4c8ff5f9a08a735c6a8a03d6b47ff850f4
Sandors <382524646@qq.com>
2019-07-18 20:54:41 +0800

    django push test

--- untitled2/urls.py
+++ untitled2/urls.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from django.urls import path,re_path,include
 
 from my_first_django import views
+from book import views
 from django.conf.urls import url
 
 urlpatterns = [
 
     # re_path(r'^blog/', include('templates.urls')),
-    re_path(r'^archives/([0-9]{4}/$)',views.year_archive,name='news-year-archive'),
-    re_path(r'time/',views.current_datetime),
-    re_path(r'index/',views.index),
-    re_path(r'moban/',views.moban),
-    re_path(r'moban1/',views.moban1),
-    re_path(r'moban2/',views.moban2),
-
+    # re_path(r'^archives/([0-9]{4}/$)',views.year_archive,name='news-year-archive'),
+    # re_path(r'time/',views.current_datetime),
+    # re_path(r'index/',views.index),
+    # re_path(r'moban/',views.moban),
+    # re_path(r'moban1/',views.moban1),
+    # re_path(r'moban2/',views.moban2),
+    # re_path(r'check_data/',views.check_data),
 
 ]
 
