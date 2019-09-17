9da06f6aea60b0f11536b9a58e7232e9483699fb
Sandors <382524646@qq.com>
2019-09-17 14:45:00 +0800

    django 20190917

--- untitled2/settings.py
+++ untitled2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'qwhjoou2!vfjv^1#0iftykahfe)v3dt&shglb4n06q^@l)d484'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
